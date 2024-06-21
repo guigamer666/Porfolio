3034d7a4904c037b80560cabf7c0c6ed7f6085e9
Guillermo <fuentesbuenosvinosguillermo@gmail.com>
2024-06-21 19:46:33 +0200

    comentarios

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -16,9 +16,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// Obtén una referencia al almacenamiento
+//Usando la inicializacion de firebase usamos el almacenamiento de firebase
 const storage = getStorage(app);
-
+//Creamos un array de  de objetos que contiene la carpeta y las imagenes dentro de la carpeta
 const folders = [
   {
     folderName: 'gestorcitas',
@@ -57,25 +57,44 @@ const folders = [
     imageNames: ['1.PNG', '2.PNG', '3.PNG', '4.PNG', '5.PNG', '6.PNG'],
   },
 ];
-
+//Creamos un metodo asincrono para obtener la url de cada imagen
 export const getImageUrls = async () => {
+  //De esas imagenes obtenemos en formato de array el identificador de todas las imagenes y convertimos
+  //ese array de identificadores a un array de strings, el identificador es el nombre de la carpeta
   const allImageUrls: { [key: string]: string[] } = {};
-
+  //Interamos el array de carpetaz
   for (const folder of folders) {
+    //en cada iteracion se obtiene el nombre de la carpeta y las imagenes dentro de la
+    //misma, esta tecnica se llama deconstruccion del array principal folders.
     const { folderName, imageNames } = folder;
+    //Almacenamos en un array de string todas las url de todas las imagenes
     const imageUrls: string[] = [];
-
+    //Dentro del bucle que itera las carpetas interamos el nombre de cada imagen
     for (const name of imageNames) {
+      //con ref retornamos la url donde se almacena la imagen
+      //a este metodo ref le pasamos el almacenamiento y despues se le pasa 2 parametros mas
+      //entre apostrofes que recibe 2 argumentos el nombre de la carpeta folderName y luego
+      //el nombre de la imagen dentro de esa carpeta name esto en conjunto forman la url
+      //de donde esta la imagen
       const imageReference = ref(storage, `${folderName}/${name}`);
 
       try {
+        //Usamos getDownloadURL que es un metodo de firebase para descargar la imagen este metodo
+        //recibe la url anterior imagereference y la descarga
         const url = await getDownloadURL(imageReference);
+        //y esa url lo metemos en el array de strings anterior imageUrls
         imageUrls.push(url);
       } catch (error) {
         console.log(error);
       }
     }
-
+    // Asignamos el array de URLs de las imágenes (imageUrls) a una propiedad en el objeto allImageUrls.
+    //El motivo por el cual tiene que recibir folderName de esta manera es primero porque es un array
+    //y segundo porque es la clave que se va a usar que es el nombre de cada carpeta
+    /*Basicamente decimos, despues de que me obtengas la url de todas las imagenes y sabemos
+   que cada imagen pertenece a una carpeta en concreto, asignamos ese array de urls a un array
+   mayor imageUrls que tendra la url de cada imagen, y como valor de la propiedad folderName(nombre de carpeta)
+   */
     allImageUrls[folderName] = imageUrls;
   }
 
